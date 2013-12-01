9512cb14cdc49734b8a38f82c13df58e2c536281
Thorsten Bruhns <tbruhns@gmx.de>
2013-12-01 19:39:09 +0100

    Default-Services nach Installation von OracleLinux5 deaktivieren

---
+++ Linux/OL5/ol5_default_services_off.sh
@@ -0,0 +1,19 @@
+chkconfig acpid off
+chkconfig bluetooth off
+chkconfig cups off
+chkconfig hidd off
+chkconfig hplip off
+chkconfig ip6tables off
+chkconfig isdn off
+chkconfig pcscd off
+chkconfig rhnsd off
+chkconfig smartd off
+chkconfig yum-updatesd off
+chkconfig avahi-daemon off
+
+chkconfig o2cb off
+chkconfig ocfs2 off
+
+
+# XEN
+chkconfig microcode_ctl off
