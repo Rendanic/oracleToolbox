76cc4ec594a29a9cd2a9d9a7612c70f42665f0a3
Thorsten Bruhns <tbruhns@gmx.de>
2013-12-01 19:37:22 +0100

    Zusaetzliche RPMs neben der Default-Installation - sehr hilfreich

---
+++ Linux/zusaetzliche_pakete.sh
@@ -0,0 +1,19 @@
+# Interessante Pakete fuer OracleLinux 5/6
+#
+yum install -y iotop iptraf strace yum-plugin-versionlock
+
+yum install -y mc expect
+
+# Tools zum Online einbinden von SCSI-Devices
+yum install -y sg3-utils lsscsi
+
+# Hilfreich fuer Remotesupport
+yum install -y rdesktop
+
+# Subversion ist immer gut zu gebrauchen
+yum install -y subversion
+
+################################################################
+# EPEL benoetigt - nicht im Repository von Oracle enthalten
+yum install -y htop
+
