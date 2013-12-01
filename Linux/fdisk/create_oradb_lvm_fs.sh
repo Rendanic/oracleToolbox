c4089a1888b12ab537f86fb7876847df33dc7c72
Thorsten Bruhns <tbruhns@gmx.de>
2013-12-01 19:34:29 +0100

    Script for crerating a volume group for oracle Databases and mounting it to /u02 and /u03

---
+++ Linux/fdisk/create_oradb_lvm_fs.sh
@@ -0,0 +1,173 @@
+#!/bin/bash
+# $Id: create_oradb_lvm_fs.sh 633 2013-01-19 19:50:41Z tbr $
+#
+# (c) Thorsten Bruhns (thorsten.bruhns@opitz-consulting.com)
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+# Das Skript erzeugt in der Volume-Gruppe 2 logical Volumes mit dem Namen
+# fralv und oradatalv
+# Es wird immer davon ausgegangen, das Oracle managed Files zum Einsatz kommen
+# => Der Mountpoint fuer die Datenbank wird immer 'gross' geschrieben - unab-
+#    haengig vom db_name!
+#
+# Die Filesysteme werden nach folgenden Schema eingehät: (db_name=TUX112)
+# oradatalv => /u02/app/oracle/oradata/TUX112
+# fralv     => /u03/app/oracle/fra/TUX112
+#
+# Parameter:
+# 1. DB_NAME
+# 2. Groesse in GB fuer oradata
+# 3. Groesse in GB fuer fra
+#
+# Beispiel:
+# create_oradb_lvs.sh TUX 80 10
+#
+
+set_env() {
+	if [ ${#} -ne 3 ]
+	then
+		echo "Es muessen 3 Parameter uebergeben werden!"
+		echo `basename $0`" <DB_NAME> <oradata groesse in GB> <fra groesse in GB>"
+		echo "Beispiel: "`basename $0`" TUX11 30 10"
+		exit 1
+	fi
+
+	oradbname=${1}
+	vggruppe=oradb${oradbname}vg
+	lvoradatasize=${2}
+	lvfrasize=${3}
+
+	# fstype ist bei SLES11 nur ext3, da ext4 nicht supported. :-(
+	rpm -qa | egrep "^SLES-for-VMware-release|sles-release" > /dev/null &2>1
+	if [ ${?} -eq 0 ]
+	then
+		# SLES gefunden
+		fstype=ext3
+	else
+		# alle anderen koennen ext4
+		fstype=ext4
+	fi
+}
+
+create_lv() {
+	vggruppe=${1}
+	lvname=${2}
+	lvsize=${3}
+	
+	# LV vorhanden?
+	lvdisplay /dev/${vggruppe}/${lvname} > /dev/null 2>&1
+	if [ ! ${?} = 0 ]
+	then
+		# NEIN
+		# => LV anlegen!
+		echo "Erzeuge Logical Volume:"
+		echo "lvcreate -n ${lvname} -L ${lvsize}g /dev/${vggruppe}" 
+		lvcreate -n ${lvname} -L ${lvsize}g /dev/${vggruppe} >/dev/null
+	else
+		echo "Logical Volume "/dev/${vggruppe}/${lvname}" vorhanden!"
+	fi
+}
+
+do_lvcreate() {
+	create_lv ${vggruppe} oradatalv ${lvoradatasize}
+	create_lv ${vggruppe} fralv ${lvfrasize}
+}
+
+
+create_extfs() {
+	vggruppe=${1}
+	lvname=${2}
+	vollabel=${3}
+
+	# nur wenn das lv tatsaechlich vorhanden ist wird versucht ein FS zu erzeugen
+	lvdisplay /dev/${vggruppe}/${lvname} > /dev/null 2>&1
+	if [  ${?} = 0 ]
+	then
+		# existiert bereits ein Filesystem?
+		# funktioniert nur zuverlassig wenn das Filesystem bereits eingehaengt ist. :-(
+		mkfs.${fstype} -n /dev/${vggruppe}/${lvname} > /dev/null 2>&1
+		if [ ${?} = 0 ]
+		then
+			# probelauf erfolgreich
+			# kein filesystem vorhanden, bestehendes filesystem ist nicht gemounted
+			# => Filesystem darf erzeugt werden!
+			echo "Erzeuge neues Filesystem auf "/dev/${vggruppe}/${lvname}
+			mkfs.${fstype} -L ${vollabel} -m 1 -q /dev/${vggruppe}/${lvname}
+		else
+			echo "gueltiges Filesystem in "/dev/${vggruppe}/${lvname}" gefunden!"
+		fi
+	fi
+}
+
+do_mkfs() {
+	# Volumelabel vom Filesystem darf nur max 16 Zeichen lang sein!
+	create_extfs ${vggruppe} oradatalv "oradb"${oradbname}"dat"
+	create_extfs ${vggruppe} fralv "oradb"${oradbname}"fra"
+}
+
+modify_fstab() {
+	lvname=${1}
+	mountpoint=${2}
+		
+	if [ ! -d ${mountpoint} ]
+	then
+		mkdir -p  ${mountpoint} 
+	fi
+
+	mountdevice="/dev/"${vggruppe}"/"${lvname}
+
+	# pruefe ob ein Mountpoint in /etc/fstab schon vorhanden ist
+	# existiert schon ein Eintrag in der fstag?
+	grep "^"${mountdevice} /etc/fstab > /dev/null 2>&1
+	if [ ! ${?} = 0 ]
+	then
+		# kein Eintrag vorhanden
+		# => Eintrag einfuegen!
+		echo "Erzeuge Eintrag fuer "${mountdevice}" in /etc/fstab"
+		echo ${mountdevice}" 	"${mountpoint}" 	"${fstype}" 	defaults 0 0" >> /etc/fstab
+	fi
+
+	# Zur Sicherheit umount
+	# => Der darf ruhig fehl schlagen - daher die Umleitung nach /dev/null
+	umount ${mountdevice} > /dev/null 2>&1
+		
+	# Mount des neuen Filesystems
+	mount ${mountdevice} > /dev/null 2>&1
+
+	# Nur wenn der Mount erfolgreich ist, wird der chown durchgefuehrt!
+	if [ ${?} = 0 ]
+	then 
+		# chown bewusst NUR auf das eingehaengte Verzeichnis und nicht auf das
+		# Verzeichnis wo 'drauf' gemountet wird
+		chown oracle ${mountpoint}
+		echo " "
+		echo "Verzeichnis erfolgreich eingehaengt:"
+		df -h ${mountpoint}
+	fi
+}
+
+do_fstab() {
+	oradbnameupper=`echo ${oradbname} | tr '[:lower:]' '[:upper:]'`
+
+	modify_fstab oradatalv 		/u02/app/oracle/oradata/${oradbnameupper}
+	modify_fstab fralv 		/u03/app/oracle/fra/${oradbnameupper}
+}
+
+set_env ${*}
+do_lvcreate 
+do_mkfs
+do_fstab
+
---
+++ Linux/fdisk/sfdisk_lvm_vg.sh
@@ -0,0 +1,137 @@
+#!/bin/bash
+# $Id: sfdisk_lvm_vg.sh 633 2013-01-19 19:50:41Z tbr $
+#
+# (c) Thorsten Bruhns (tbruhns@gmx.de)
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+# ICH UEBERNEHME KEINE GARANTIE FUER DIE FUNKTION DES SKRIPTES
+# FALSCHES ANWENDEN KANN ERHEBLICHEN DATENVERLUST NACH SICH ZIEHEN!
+
+# Das Skript erzeugt auf einem Device eine 1. primaere Partition
+# ueber den gesammten Bereich mit dem Typ LVM
+# Disk Alignment wird dabei beruecksichtigt.
+# sfdisk muss als Befehl verfuegbar sein!
+#
+# Im Anschluss wird eine Volume-Gruppe angelegt. Sollte die Gruppe
+# bereits vorhanden sein wird sie entsprechend erweitert.
+#
+# ACHTUNG!!!
+# Eine Disk ohne Partition wird dabei ueberschrieben. Disks mit
+# bestehenden Partitionen werden nicht veraendert!
+#
+# Parameter 1: Device fuer sfdisk
+# Parameter 2: Name der Volume-Gruppe
+
+# todo: Ueberpruefung mittels blkid zur Sicherrung das kein Device genutzt wird!
+
+set_env()
+{
+	# we need 2 parameter!
+	if [ ${#} -ne 2 ]
+	then
+		echo " "
+		echo `basename $0`" <physical Device> <Volume-Group>"
+		echo " "
+		echo "Example: "`basename $0`" /dev/xvds testvg"
+		echo " "
+		echo "This script creates a 1st primary partition on given disc for LVM when no partition table is existing"
+		echo "The partition will labeled for LVM after then."
+		echo "Finaly we create a Volume-Group with the new partition or extend an existing Volume-Group"
+		exit 99
+	fi
+	sdpartition=${1}
+	volumegroup=${2}
+	
+	pvpartition=${sdpartition}1
+
+}
+do_create_pv()
+{
+	# check for an existing partition
+	# sfdisk prints the device only 1 times when no partition-table is existing
+	countpart=`sfdisk -lR ${sdpartition} | grep ${sdpartition} | wc -l` 
+	if [ ${countpart:-0} -eq 1 ]
+	then
+		# todo!!
+		blkid ${sdpartition} > /dev/null
+		retcode=${?}
+		if [ ${retcode} -eq 0 ]
+		then
+			# found a valid filesystem or something else
+			# => we can't create a parti	tion table here
+			echo "blkid (blkid ${sdpartition}) found something on  "${sdpartition}
+			echo "Aborting script!"
+			exit 20
+		fi
+		
+		pvdisplay ${sdpartition} > /dev/null 2>&1
+		if [ ${retcode} -eq 0 ]
+		then
+			# We found a valid physical volume on the disc!
+			echo "Valid Label for LVM found on "${sdpartition}
+			echo "Aborting script!"
+			exit 30
+		else
+			# we have no physical volume on this disc!
+
+			# => We can create a partition!
+			echo "2048,,8e"|sfdisk -uS -q --force ${sdpartition} 
+			if [ ${?} -eq 0 ]
+			then
+				blkid -g
+				# sleep 2 seconds, because SLES11 SP2 doesn't find the partition for pvcreate
+				sleep 2
+	
+				# we only create a physical volume when sfdisk was able to create the partition!
+				# we can create the physical volume on the new partition
+				echo "Creating a Label for LVM on "${pvpartition}
+				pvcreate ${pvpartition}
+			fi
+		fi
+	fi
+}
+
+do_make_vg()
+{
+	# is the device for LVM a block-device?
+	if [ ! -b ${pvpartition} ]
+	then
+		echo "Cannot work on Volume-Group "${volumegroup}" because partition "${pvpartition}" is not a block device!"
+		echo "Skript aborted!"
+		exit 10
+	fi
+
+	# extend existing Volume Group or create a new one
+	echo "Check for an existing Volume-Group"
+	vgdisplay ${volumegroup} > /dev/null 2>&2
+	retcode=${?}
+	if [ ${retcode} -eq 0 ]
+	then
+		# Volume Group exists!
+		vgextend ${volumegroup} ${pvpartition}
+	else
+		# creating a new Volume Group
+		vgcreate ${volumegroup} ${pvpartition}
+		# change max number of physical volumes for the new volume group
+		echo "Change the maximum number of physical disks for the new Volume-Group"
+		vgchange ${volumegroup} -p 0
+	fi
+}
+
+set_env ${*}
+do_create_pv
+do_make_vg
+ 
