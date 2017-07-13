97da01c1aac066af7efdd80f6cfe9217df1faf40
Thorsten Bruhns <oracle@omd110.local>
2017-07-13 07:40:10 +0200

    some modifications in trigger

--- datapump/trg_login_impdp.sql
+++ datapump/trg_login_impdp.sql
@@ -1,36 +1,30 @@
 
 
 CREATE OR REPLACE TRIGGER system.trg_login_impdp
-  AFTER LOGON ON DATABASE
+  AFTER LOGON ON database when (user='IMPDP')
 
 DECLARE
     server_host     varchar2(1000) := lower(SYS_CONTEXT('USERENV', 'HOST'));
     host            varchar2(1000) := lower(SYS_CONTEXT('USERENV', 'SERVER_HOST'));
     module          varchar2(1000) := lower(SYS_CONTEXT('USERENV', 'MODULE'));
 
-    module_allowwd  varchar2(30)   := 'impdp';
+    module_allowwd  varchar2(30)   := 'udi';
 
 BEGIN
     IF instr(server_host, '.') > 0 THEN
-        server_host := substr(server_host, 1, instr(server_host, '.')-2);
+        server_host := substr(server_host, 1, instr(server_host, '.')-1);
     END IF;
 
     IF instr(host, '.') > 0 THEN
         host := substr(host, 1, instr(host, '.')-1);
     END IF;
 
+    -- Login from host where oracle is running is allowrd
+    -- => Requirred for administration of database link in SQLPlus
+    if host <> server_host 
+      and instr(module, module_allowwd) <> 1 THEN
 
-    IF sys.login_user = 'IMPDP' THEN
-
-        -- Login from host where oracle is running is allowrd
-        -- => Requirred for administration of database link in SQLPlus
-        if host = server_host 
-          or instr(module, module_allowwd) = 1 THEN
-            NULL;
-        ELSE
-
-            RAISE_APPLICATION_ERROR(-20001,'Login not allowed for user ' || user || ' with different hostname server_host: ' || server_host || ' host: ' || host || ' Module: ' || module || instr(module, module_allowwd));
-        END IF;
+        RAISE_APPLICATION_ERROR(-20001,'Login not allowed for user ' || user || ' host: ' || host || ' with Module: ' || module);
     END IF;
 END;
 /
